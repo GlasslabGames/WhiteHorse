f3ba816f0ddf3ef1d09833f71c850399bdfe329e
Rose Abernathy <rose@instituteofplay.org>
2014-09-02 14:50:00 -0700

    More stuff added to the demo scene.

---
+++ Unity/Assets/Scripts/Demo/DemoEnemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoEnemy : MonoBehaviour {
+	private DemoEnemyModel m_model;
+  public DemoEnemyModel Model {
+    get { return m_model; }
+    set {
+      m_model = value;
+      Utility.NextFrame( Refresh );
+    }
+  }
+
+  public void Refresh() {
+    transform.localPosition = new Vector3( Model.Position[0], Model.Position[1], 0);
+    transform.localScale    = new Vector3( Model.Scale[0], Model.Scale[1], 1);
+  }
+
+	public void Interact() {
+    DemoManager.Instance.Dialog.Show(transform.position, Model.Name, DemoBarkModel.GetModel(Model.Bark).Text);
+	}
+}
