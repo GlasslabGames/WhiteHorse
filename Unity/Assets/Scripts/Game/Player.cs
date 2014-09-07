2684291cd9365eb78f756449934d155e3266635c
Rose Abernathy <rose@glasslabgames.org>
2014-09-07 01:40:12 -0400

    New color scheme!

--- Unity/Assets/Scripts/Game/Player.cs
+++ Unity/Assets/Scripts/Game/Player.cs
@@ -30,14 +30,15 @@ public class Player : MonoBehaviour
       m_playerColor = Color.red;
       m_opponentColor = Color.blue;
       GameObjectAccessor.Instance.RedPlayerMarker.SetActive( true );
-      GameObjectAccessor.Instance.ToggleWorkerButton.GetComponent< UITexture >().color = State.redStateColorInactive;
+      GameObjectAccessor.Instance.ToggleWorkerButton.GetComponent< UITexture >().color = State.redStateColor;
+      GameObjectAccessor.Instance.ToggleWorkerButton.GetComponent< UITexture >().alpha = 0.5f;
     }
     else
     {
       m_playerColor = Color.blue;
       m_opponentColor = Color.red;
       GameObjectAccessor.Instance.BluePlayerMarker.SetActive( true );
-      GameObjectAccessor.Instance.ToggleWorkerButton.GetComponent< UITexture >().color = State.blueStateColorInactive;
+	  GameObjectAccessor.Instance.ToggleWorkerButton.GetComponent< UITexture >().alpha = 0.5f;
     }
   }
 
@@ -47,11 +48,11 @@ public class Player : MonoBehaviour
 
     if( m_leaning == Leaning.Red )
     {
-      GameObjectAccessor.Instance.ToggleWorkerButton.GetComponent< UITexture >().color = m_campaignWorkerSelected ? State.redStateColor : State.redStateColorInactive;
+      GameObjectAccessor.Instance.ToggleWorkerButton.GetComponent< UITexture >().alpha = m_campaignWorkerSelected ? 1 : 0.5f;
     }
     else
     {
-      GameObjectAccessor.Instance.ToggleWorkerButton.GetComponent< UITexture >().color = m_campaignWorkerSelected ? State.blueStateColor : State.blueStateColorInactive;
+      GameObjectAccessor.Instance.ToggleWorkerButton.GetComponent< UITexture >().alpha = m_campaignWorkerSelected ? 1 : 0.5f;
     }
   }
 }
