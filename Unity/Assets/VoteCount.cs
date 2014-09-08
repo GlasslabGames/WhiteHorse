c8dc46dda35332239f66d73ca752eb06fad43513
Rose Abernathy <rose@glasslabgames.org>
2014-09-07 20:38:36 -0400

    Developing class to scroll votes to the next value.

---
+++ Unity/Assets/VoteCount.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoteCount : MonoBehaviour {
+	private int m_current;
+	private int m_target;
+	//private float m_time;
+	//private float m_waitTime;
+	private UILabel m_label;
+
+	void Awake() {
+		m_label = GetComponent<UILabel> ();
+	}
+
+	public void Set(int target) {
+		m_target = target;
+	}
+
+	void Update() {
+		if (m_current != m_target) {
+			if (m_current < m_target) m_current ++;
+			else m_current --;
+
+			m_label = m_target;
+		}
+	}
+
+}
